dae3e7d11072f0eaf34cd883681752c8e5b078b5
Richard Beasley <richardbeasley27@gmail.com>
2015-10-07 18:31:01 -0600

    [#104389454] Added Readme.md irb_calculator.md ex3.rb ex3_floating.rb

---
+++ Chapter_3/Readme.md
@@ -0,0 +1,4 @@
+The math in the calculator isn't working correctly because
+the numbers that dont divide evenly into the other number will come out a
+ whole number and will be off. because in order to get a fraction o
+ ut you have to use a floating number.
---
+++ Chapter_3/ex3_floating.rb
@@ -0,0 +1,27 @@
+# tells computer to count the chickens
+puts "I will now count my chickens:"
+# does math to get the answer for the number of animals
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+# counts the eggs
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# tells that the statement is true
+puts "Is is true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+# tells you the answer to the problem it shows
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+# outputs string
+puts "Oh, that's why it's false."
+# outputs string
+puts "How about some more."
+# tells if > < or =
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5<= -2}"
+
+puts 39 / 6.0
+puts 654 / 9.0
